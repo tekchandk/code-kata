4a31b05b9fad5a189610cfddb4c1fea7e20c55a9
Tek <tekchandy14@gmail.com>
2021-01-06 11:32:53 +0530

    Removed codebase

--- src/PriceContract.java
+++
@@ -1,7 +0,0 @@
-import models.Product;
-
-public interface PriceContract {
-    int getPriceWithoutOffer(int checkoutQuantityProduct, Product product);
-    int getPriceWithOffer(int checkoutQuantityProduct, Product product);
-    int getTotalPrice(String[] itemNames);
-}
